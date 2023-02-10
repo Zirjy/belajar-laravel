b98473b29485da9a7518d85d2cc3f51ba577a2f4
ZirjyZakwanFayyadhiya <zirjyzakwan@gmail.com>
2023-02-10 09:26:04 +0700

    Verify Email

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -58,6 +58,10 @@ class FortifyServiceProvider extends ServiceProvider
             return view('auth.resetpassword', ['request' => $request]);
         });
 
+        Fortify::verifyEmailView(function(){
+            return view('auth.verify-email');
+        });
+
         
     }
 }
